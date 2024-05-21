7c213ea99631609f5a3210740e30b9ea659a3c73
emchoBA <93426572+emchoBA@users.noreply.github.com>
2024-05-21 22:19:52 +0300

    added calorie calculation with user inputs

--- tidymeal/views.py
+++ tidymeal/views.py
@@ -9,10 +9,8 @@ from random import uniform as rnd
 
 from tidymeal.diet_recom import get_recommendations, custom_formatting
 
-#initial values are set as -1 for easy error debug
-user_age = -1
-user_weight = -1
-user_height = -1
+#initial values are set as 1 for easy error debug
+
 def say_hello(request):
     return render(request, 'homepage.html')
 # Create your views here.
@@ -27,12 +25,40 @@ def user_home_page(request):
     return render(request, 'userhomepage.html')
 
 def displayInfopage(request):
-    global user_age, user_weight, user_height
+
     user_age = request.POST.get('age')
     user_weight = request.POST.get('weight')
     user_height = request.POST.get('height')
+    user_activity = request.POST.get('activity')
+    user_plan = request.POST.get('plan')
 
-    result_list = get_recommendations(1500)
+    activity_multiplier = 1
+    plan_modifier = 0
+
+    BRM = 88.362 + (13.397 * float(user_weight)) + (4.799 * float(user_height)) - (5.677 * float(user_age))
+
+    match user_plan:
+        case "low":
+            plan_modifier = -300
+        case "medium":
+            plan_modifier = 300
+        case "high":
+            plan_modifier = 0
+
+    match user_activity:
+        case "none":
+            activity_multiplier = 1.2
+        case "light":
+            activity_multiplier = 1.375
+        case "moderate":
+            activity_multiplier = 1.55
+        case "active":
+            activity_multiplier = 1.75
+
+    calories = (BRM * activity_multiplier) + plan_modifier
+
+    print(calories)
+    result_list = get_recommendations(calories)
     breakfast_list, lunch_list, dinner_list = custom_formatting(result_list)
 
     context = {
